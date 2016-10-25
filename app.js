4bb25bd212cedcd4a842dc5a81ec2632743ab65e
4fthawaiian <brad@projectpixelpress.com>
2016-10-25 14:02:17 -0500

    cover image support

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
 var app = express();
+
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('pages/index');
@@ -42,18 +46,37 @@ app.get('/current', function(req,res) {
     // variant             string "Pitbull"
     // variant             string "http://www.pandora.com/pitbull/greenlight-single/greenlight-feat-flo-rida-lunchmoney-lewis?dc=175&ad=0:40:1:63103::0:0:0:1:609:017:MO:29510:2:0:0:0:416:0"
     // we only want lines 1-2
+    // can we get the cover image? class="img_cvr"
     var lines = stdout.split("\n");
     var title = lines[1].split("string")[1];
     var artist = lines[2].split("string")[1];
+    var info_url = lines[3].split("string")[1].trim().replace(/"/g,'');
     var response_object = {
       artist: artist.trim().replace(/"/g,''),
       title: title.trim().replace(/"/g,'')
     }
     console.log("response: %o",response_object);
-    res.json(response_object);
+    get_cover(info_url,function(out) {
+      response_object["image"] = out;
+      res.json(response_object);
+    });
   });
 });
 
+var get_cover = function(url,cb) {
+  request(url, function(error, response, html){
+    if(!error){
+      console.log('getting cover');
+      var $ = cheerio.load(html);
+      var cover_img = $('.img_cvr');
+      cover_img = cover_img[0]['attribs']['src'];
+      cb(cover_img);
+    } else {
+      console.log('u wot m8: %o',error);
+      cb("fuck: %o",error);
+    }
+  })
+}
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
